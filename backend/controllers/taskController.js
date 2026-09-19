44a7026e4a751dbecedab5fc75ef58f20cedb18b
prashant3pc <chaudharypuspa502@gmail.com>
2026-09-19 14:55:33 +0545

    now thinking to apply admin pages as well

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -14,17 +14,23 @@ export const createTask = asyncHandler(async (req, res) => {
     data: newTask,
   });
 });
+//OWNERSHIP ONLY THIS
+// export const getTasks = asyncHandler(async (req, res) => {
 
+//   const tasks = await Task.find({
+//     user: req.user.id,
+//   });
+
+//   return res.json({
+//     success: true,
+//     message: "All your tasks is here",
+//     data: tasks,
+//   });
+
+// });
 export const getTasks = asyncHandler(async (req, res) => {
-  // const tasks = await Task.find({
-  //   user: req.user.id,
-  // });
-  // res.json({
-  //   success: true,
-  //   message: "All your tasks is here",
-  //   data: tasks,
-  // });
   const filter = { user: req.user.id };
+
   if (req.query.status) {
     filter.status = req.query.status;
   }
